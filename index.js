638f1d0effef9e4ec1eeb930b244999a752f97fe
Cynthia <cynthia.abuya@student.moringaschool.com>
2025-03-13 11:36:53 +0300

    variable lab

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Code your solution in this file!
-const companyName ='Scuber';
-companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
-mostProfitableNeighborhood = 'Chelsea';
-let companyCeo ='Susan Smith';
-companyCeo = 'Susan Smith';
+// const companyName ='Scuber';
+// companyName = 'Scuber';
+// let mostProfitableNeighborhood = 'Chelsea';
+// mostProfitableNeighborhood = 'Chelsea';
+// let companyCeo ='Susan Smith';
+// companyCeo = 'Susan Smith';
+
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
